581f47a5718430525875d218aa9badbca3f6fa3d
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-09-15 14:25:55 +0500

    Add static

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 python manage.py migrate --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py initadmin
 
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
