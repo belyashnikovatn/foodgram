ff6466d951581bb379014df3398d68695f35434e
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-09-16 19:09:29 +0500

    Fix entrypoint extra symbol

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-python manage.py migrate --no-input
-python manage.py initadmin
-
-gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
+python manage.py makemigrations && python manage.py migrate --noinput && python manage.py collectstatic --no-input && python manage.py initadmin && gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
