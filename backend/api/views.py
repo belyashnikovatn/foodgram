0f6f0c865424fd427081b21376ce225443f4c0a2
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-09-30 12:45:10 +0500

    pagination should work

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as UVS
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (AllowAny, IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
